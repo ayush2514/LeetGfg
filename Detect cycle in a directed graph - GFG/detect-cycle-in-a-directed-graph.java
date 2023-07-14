e533abfc426c83033a4c5360122f5973efb12036
ayush2514 <72273642+ayush2514@users.noreply.github.com>
2023-07-14 18:58:35 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -33,31 +33,37 @@ class DriverClass {
 
 class Solution {
     
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj,int node,boolean v[], boolean pv[]) {
-        v[node]=true;
-        pv[node]=true;
-        
-        for(int x:adj.get(node)) {
-            if(!v[x] && dfs(adj,x,v,pv))
-            return true;
-            else if(pv[x])
-            return true;
-        }
-        pv[node]=false;
-        return false;
-    }
     // Function to detect cycle in a directed graph.
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
         // code here
-        boolean vis[]=new boolean[V];
-        boolean pathVis[]=new boolean[V];
+        int indegree[]=new int[V];
         
         for(int i=0;i<V;i++) {
-            if(!vis[i]) {
-                if(dfs(adj,i,vis,pathVis)==true)
-                return true;
-            }
+            for(int x:adj.get(i))
+            indegree[x]++;
         }
-        return false;
+        
+        Queue<Integer> q=new LinkedList<>();
+        
+        for(int i=0;i<V;i++) {
+            if(indegree[i]==0)
+            q.add(i);
+        }
+        int count=0;
+        while(!q.isEmpty()) {
+            int node=q.peek();
+            q.remove();
+            count++;
+          
+          for(int curr : adj.get(node))
+          {
+              indegree[curr]--;
+              if(indegree[curr]==0)
+              q.add(curr);
+          }
+        }
+        if(count==V) return false;
+        return true;
+
     }
 }
